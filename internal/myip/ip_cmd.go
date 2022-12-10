cfcdec4381ddb7d7ec821acb8a1b4c027866c94b
tae2089 <tae2089002@gmail.com>
2022-12-11 08:19:32 +0900

    feature: what is my ip 기능 추가

--- internal/myip/ip_cmd.go
+++ internal/myip/ip_cmd.go
@@ -1,1 +1,14 @@
 package myip
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var Myipcmd = &cobra.Command{
+	Use:     "version",
+	Aliases: []string{"rev"},
+	Short:   "print the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		GetMyIp()
+	},
+}
